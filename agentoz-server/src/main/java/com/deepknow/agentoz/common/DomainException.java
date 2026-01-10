c1324d547875b56bd1f6a86ee1af12aed79decc6
张紫豪 <1131220792@qq.com>
2026-01-11 01:02:29 +0800

    refactor: Remove unnecessary blank line in DomainException class

--- agentoz-server/src/main/java/com/deepknow/agentoz/common/DomainException.java
+++ agentoz-server/src/main/java/com/deepknow/agentoz/common/DomainException.java
@@ -17,6 +17,7 @@ public class DomainException extends RuntimeException {
 
     private final String errorCode;
 
+
     public DomainException(String errorCode, String message) {
         super(message);
         this.errorCode = errorCode;
